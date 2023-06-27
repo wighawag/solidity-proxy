d2b82eec881086e9edbd6964c2158a0b3614369f
wighawag <wighawag@gmail.com>
2023-06-27 07:16:01 +0100

    Router now use 0-index

--- solc_0.8/router/Router10X60.sol
+++ solc_0.8/router/Router10X60.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.0;
 /// @author Ronan Sandford
 /// @notice Create an immutable route and delegate function call to their respective implementations
 contract Router10X60 {
+	address internal immutable implementation_00;
 	address internal immutable implementation_01;
 	address internal immutable implementation_02;
 	address internal immutable implementation_03;
@@ -14,10 +15,10 @@ contract Router10X60 {
 	address internal immutable implementation_07;
 	address internal immutable implementation_08;
 	address internal immutable implementation_09;
-	address internal immutable implementation_10;
 
 	address internal immutable fallback_implementation;
 
+	uint256 internal immutable sigs_00;
 	uint256 internal immutable sigs_01;
 	uint256 internal immutable sigs_02;
 	uint256 internal immutable sigs_03;
@@ -27,16 +28,18 @@ contract Router10X60 {
 	uint256 internal immutable sigs_07;
 	uint256 internal immutable sigs_08;
 	uint256 internal immutable sigs_09;
-	uint256 internal immutable sigs_10;
 
 	uint256 internal immutable sigs_count;
 
 	// This is the interface for routes specification
 	struct Routes {
 		address[] implementations;
-		// 255 implementation max as index = 0 means no implementation (allow bytes4(0) to be valid)
-		// first 4 bytes are the bytes4, rightmost is the (index + 1) for the implementation
-		// need to be ordere
+		/// The sigmap is represented as an array of entry
+		// each entry is 5 bytes
+		/// the upmost bytes are the bytes4 function selector
+		// the lowest byte represent the index in the implementation's address array provided aling
+		// This measn there is at max 256 implementation possible
+		// The entry needs to be ordered so binary search can be executed on them
 		bytes5[] sigMap;
 		address fallbackImplementation;
 	}
@@ -53,7 +56,8 @@ contract Router10X60 {
 		require(numSigs <= 60, 'MAX_SIGS_REACHED');
 
 		{
-			address tmp_implementation_01 = routes.implementations[0];
+			address tmp_implementation_00 = routes.implementations[0];
+			address tmp_implementation_01;
 			address tmp_implementation_02;
 			address tmp_implementation_03;
 			address tmp_implementation_04;
@@ -62,35 +66,35 @@ contract Router10X60 {
 			address tmp_implementation_07;
 			address tmp_implementation_08;
 			address tmp_implementation_09;
-			address tmp_implementation_10;
 			if (numImpl > 1) {
-				tmp_implementation_02 = routes.implementations[1];
+				tmp_implementation_01 = routes.implementations[1];
 			}
 			if (numImpl > 2) {
-				tmp_implementation_03 = routes.implementations[2];
+				tmp_implementation_02 = routes.implementations[2];
 			}
 			if (numImpl > 3) {
-				tmp_implementation_04 = routes.implementations[3];
+				tmp_implementation_03 = routes.implementations[3];
 			}
 			if (numImpl > 4) {
-				tmp_implementation_05 = routes.implementations[4];
+				tmp_implementation_04 = routes.implementations[4];
 			}
 			if (numImpl > 5) {
-				tmp_implementation_06 = routes.implementations[5];
+				tmp_implementation_05 = routes.implementations[5];
 			}
 			if (numImpl > 6) {
-				tmp_implementation_07 = routes.implementations[6];
+				tmp_implementation_06 = routes.implementations[6];
 			}
 			if (numImpl > 7) {
-				tmp_implementation_08 = routes.implementations[7];
+				tmp_implementation_07 = routes.implementations[7];
 			}
 			if (numImpl > 8) {
-				tmp_implementation_09 = routes.implementations[8];
+				tmp_implementation_08 = routes.implementations[8];
 			}
 			if (numImpl > 9) {
-				tmp_implementation_10 = routes.implementations[9];
+				tmp_implementation_09 = routes.implementations[9];
 			}
 
+			implementation_00 = tmp_implementation_00;
 			implementation_01 = tmp_implementation_01;
 			implementation_02 = tmp_implementation_02;
 			implementation_03 = tmp_implementation_03;
@@ -100,10 +104,10 @@ contract Router10X60 {
 			implementation_07 = tmp_implementation_07;
 			implementation_08 = tmp_implementation_08;
 			implementation_09 = tmp_implementation_09;
-			implementation_10 = tmp_implementation_10;
 		}
 
 		{
+			uint256 tmp_sigs_00;
 			uint256 tmp_sigs_01;
 			uint256 tmp_sigs_02;
 			uint256 tmp_sigs_03;
@@ -113,7 +117,6 @@ contract Router10X60 {
 			uint256 tmp_sigs_07;
 			uint256 tmp_sigs_08;
 			uint256 tmp_sigs_09;
-			uint256 tmp_sigs_10;
 			
 			uint256 lastPair;
 			for (uint256 i = 0; i < numSigs; i++) {
@@ -121,27 +124,28 @@ contract Router10X60 {
 				require(lastPair < pair, "NOT_IN_ORDER");
 				lastPair = pair;
 				if (i < 6) {
-					tmp_sigs_01 = tmp_sigs_01 | (pair << (i * 40));
+					tmp_sigs_00 = tmp_sigs_00 | (pair << (i * 40));
 				} else if (i < 12) {
-					tmp_sigs_02 = tmp_sigs_02 | (pair << ((i - 6) * 40));
+					tmp_sigs_01 = tmp_sigs_01 | (pair << ((i - 6) * 40));
 				} else if (i < 18) {
-					tmp_sigs_03 = tmp_sigs_03 | (pair << ((i - 12) * 40));
+					tmp_sigs_02 = tmp_sigs_02 | (pair << ((i - 12) * 40));
 				} else if (i < 24) {
-					tmp_sigs_04 = tmp_sigs_04 | (pair << ((i - 18) * 40));
+					tmp_sigs_03 = tmp_sigs_03 | (pair << ((i - 18) * 40));
 				} else if (i < 30) {
-					tmp_sigs_05 = tmp_sigs_05 | (pair << ((i - 24) * 40));
+					tmp_sigs_04 = tmp_sigs_04 | (pair << ((i - 24) * 40));
 				} else if (i < 36) {
-					tmp_sigs_06 = tmp_sigs_06 | (pair << ((i - 30) * 40));
+					tmp_sigs_05 = tmp_sigs_05 | (pair << ((i - 30) * 40));
 				} else if (i < 42) {
-					tmp_sigs_07 = tmp_sigs_07 | (pair << ((i - 36) * 40));
+					tmp_sigs_06 = tmp_sigs_06 | (pair << ((i - 36) * 40));
 				} else if (i < 48) {
-					tmp_sigs_08 = tmp_sigs_08 | (pair << ((i - 42) * 40));
+					tmp_sigs_07 = tmp_sigs_07 | (pair << ((i - 42) * 40));
 				} else if (i < 54) {
-					tmp_sigs_08 = tmp_sigs_09 | (pair << ((i - 48) * 40));
+					tmp_sigs_07 = tmp_sigs_08 | (pair << ((i - 48) * 40));
 				} else {
-					tmp_sigs_08 = tmp_sigs_10 | (pair << ((i - 54) * 40));
+					tmp_sigs_07 = tmp_sigs_09 | (pair << ((i - 54) * 40));
 				}
 			}
+			sigs_00 = tmp_sigs_00;
 			sigs_01 = tmp_sigs_01;
 			sigs_02 = tmp_sigs_02;
 			sigs_03 = tmp_sigs_03;
@@ -151,7 +155,6 @@ contract Router10X60 {
 			sigs_07 = tmp_sigs_07;
 			sigs_08 = tmp_sigs_08;
 			sigs_09 = tmp_sigs_09;
-			sigs_10 = tmp_sigs_10;
 
 			sigs_count = numSigs;
 		}
@@ -168,25 +171,25 @@ contract Router10X60 {
 			uint256 i = (left + right) / 2;
 			uint40 pair;
 			if (i < 6) {
-				pair = uint40((sigs_01 >> (i * 40)) & 0xFFFFFFFFFF);
+				pair = uint40((sigs_00 >> (i * 40)) & 0xFFFFFFFFFF);
 			} else if (i < 12) {
-				pair = uint40((sigs_02 >> ((i - 6) * 40)) & 0xFFFFFFFFFF);
+				pair = uint40((sigs_01 >> ((i - 6) * 40)) & 0xFFFFFFFFFF);
 			} else if (i < 18) {
-				pair = uint40((sigs_03 >> ((i - 12) * 40)) & 0xFFFFFFFFFF);
+				pair = uint40((sigs_02 >> ((i - 12) * 40)) & 0xFFFFFFFFFF);
 			} else if (i < 24) {
-				pair = uint40((sigs_04 >> ((i - 18) * 40)) & 0xFFFFFFFFFF);
+				pair = uint40((sigs_03 >> ((i - 18) * 40)) & 0xFFFFFFFFFF);
 			} else if (i < 30) {
-				pair = uint40((sigs_05 >> ((i - 24) * 40)) & 0xFFFFFFFFFF);
+				pair = uint40((sigs_04 >> ((i - 24) * 40)) & 0xFFFFFFFFFF);
 			} else if (i < 36) {
-				pair = uint40((sigs_06 >> ((i - 30) * 40)) & 0xFFFFFFFFFF);
+				pair = uint40((sigs_05 >> ((i - 30) * 40)) & 0xFFFFFFFFFF);
 			} else if (i < 42) {
-				pair = uint40((sigs_07 >> ((i - 36) * 40)) & 0xFFFFFFFFFF);
+				pair = uint40((sigs_06 >> ((i - 36) * 40)) & 0xFFFFFFFFFF);
 			}  else if (i < 48) {
-				pair = uint40((sigs_08 >> ((i - 42) * 40)) & 0xFFFFFFFFFF);
+				pair = uint40((sigs_07 >> ((i - 42) * 40)) & 0xFFFFFFFFFF);
 			}  else if (i < 54) {
-				pair = uint40((sigs_09 >> ((i - 48) * 40)) & 0xFFFFFFFFFF);
+				pair = uint40((sigs_08 >> ((i - 48) * 40)) & 0xFFFFFFFFFF);
 			}  else {
-				pair = uint40((sigs_10 >> ((i - 54) * 40)) & 0xFFFFFFFFFF);
+				pair = uint40((sigs_09 >> ((i - 54) * 40)) & 0xFFFFFFFFFF);
 			}
 			uint32 value = uint32(pair >> 8);
 			if (value > sig) {
@@ -194,32 +197,32 @@ contract Router10X60 {
 			} else if (value < sig) {
 				left = i + 1;
 			} else {
-				implementationIndex = pair & 0xFF;
+				implementationIndex = uint256(pair & 0xFF) + 1;
 				break;
 			}
 		}
 
 		address implementation;
 		if (implementationIndex == 1) {
-			implementation = implementation_01;
+			implementation = implementation_00;
 		} else if (implementationIndex == 2) {
-			implementation = implementation_02;
+			implementation = implementation_01;
 		} else if (implementationIndex == 3) {
-			implementation = implementation_03;
+			implementation = implementation_02;
 		} else if (implementationIndex == 4) {
-			implementation = implementation_04;
+			implementation = implementation_03;
 		} else if (implementationIndex == 5) {
-			implementation = implementation_05;
+			implementation = implementation_04;
 		} else if (implementationIndex == 6) {
-			implementation = implementation_06;
+			implementation = implementation_05;
 		} else if (implementationIndex == 7) {
-			implementation = implementation_07;
+			implementation = implementation_06;
 		} else if (implementationIndex == 8) {
-			implementation = implementation_08;
+			implementation = implementation_07;
 		} else if (implementationIndex == 9) {
-			implementation = implementation_09;
+			implementation = implementation_08;
 		} else if (implementationIndex == 10) {
-			implementation = implementation_10;
+			implementation = implementation_09;
 		} else {
 			implementation = fallback_implementation;
 		}
